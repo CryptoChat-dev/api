9899e820128c592f494d0f5783c6415465d8b13c
httpjamesm <github@httpjames.space>
2021-06-09 03:06:16 -0400

    server code

---
+++ server.js
@@ -0,0 +1,49 @@
+// Imports
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const compression = require('compression');
+const minify = require('express-minify');
+
+// File imports
+
+const config = require('./config.js');
+
+// Express middlewares
+
+app.use(compression());
+app.use(minify());
+app.use(express.static('public'));
+
+// API Routes
+
+app.get('*', function (req, res) {
+    // The 404 Route (ALWAYS Keep this as the last route)
+    res.redirect('https://cryptochat.dev');
+});
+
+
+// SocketIO
+
+io.on('connection', (connection) => {
+    connection.on('chat event', (data) => {
+        try {
+            JSON.parse(x);
+        } catch (e) {
+            json = false
+        }
+        if (typeof data === 'object' || json == true) {
+            io.emit('my response', data);
+            return
+        }
+        console.log("Event was rejected." + data)
+    });
+});
+
+// Server start
+
+server.listen(config.webserver.port, config.webserver.host, () => {
+    console.log('listening on *:' + config.webserver.port);
+});
