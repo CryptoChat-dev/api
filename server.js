0a34d1eb0adfdd5bbcab9907177fd15c515d6d6a
httpjamesm <github@httpjames.space>
2021-06-10 20:16:30 -0400

    raise default socket.io limit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    maxHttpBufferSize: 6e6
+});
 const compression = require('compression');
 const minify = require('express-minify');
 
