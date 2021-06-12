4ba912a000989d032f936736ed04f8a7edede9a3
httpjamesm <github@httpjames.space>
2021-06-11 21:55:19 -0400

    configurable max socket size

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, {maxHttpBufferSize: 6e6});
 const compression = require('compression');
 const minify = require('express-minify');
 var ss = require('socket.io-stream');
@@ -24,6 +23,7 @@ app.get('*', function (req, res) { // The 404 Route (ALWAYS Keep this as the las
     res.redirect(config.webserver.splash);
 });
 
+const io = require('socket.io')(server, {maxHttpBufferSize: config.api.maxSize});
 
 // SocketIO
 
@@ -72,12 +72,7 @@ io.on('connection', (socket) => {
 
     socket.on('leave', (data) => {
         socket.disconnect();
-        try {
-            JSON.parse(x);
-        } catch (e) {
-            json = false
-        }
-        if (typeof data === 'object' || json == true) {
+        if (typeof data === 'object') {
             var now = new Date();
             if (data.roomName === null || data.user_name === undefined) {
                 console.log(`${now} - Event had invalid fields.`)
@@ -91,50 +86,28 @@ io.on('connection', (socket) => {
         console.log(`${now} - Event was rejected: ${data}`)
     });
 
-    // socket.on('file event', (data) => {
-    //     if (config.api.fileUpload === true) {
-    //         try {
-    //             JSON.parse(x);
-    //         } catch (e) {
-    //             json = false
-    //         }
-    //         if (typeof data === 'object' || json == true) {
-    //             var now = new Date();
-    //             if (data.roomName === undefined || data.uid === undefined || data.user_name === undefined || data.name === undefined || data.type === undefined || data.data === undefined) {
-    //                 console.log(`${now} - Event had invalid fields.`)
-    //                 return;
-    //             }
-    //             io.to(data.roomName).emit('file response', {
-    //                 user_name: data.user_name,
-    //                 name: data.name,
-    //                 type: data.type,
-    //                 data: data.data,
-    //                 uid: data.uid
-    //             });
-    //             return;
-    //         }
-    //         console.log(`${now} - Event was rejected.`)
-    //     }
-    // })
-
     ss(socket).on('file event', function(stream, data) {
         var now = new Date();
-        console.log(`${now} - File event received: ${data.name}`)
+
+        console.log(`${now} - File event received`)
+        
+        try {
+            io.to(data.roomName).emit('file response', {
+                user_name: data.user_name,
+                name: data.name,
+                type: data.type,
+                data: data.data
+            });
+        } catch(err) {
+            console.log(`${now} - Event had invalid fields.`);
+        }
+        
         io.to(socket.id).emit('file progress', {
             finished: true
-        });
-        
-        io.to(data.roomName).emit('file response', {
-            user_name: data.user_name,
-            name: data.name,
-            type: data.type,
-            data: data.data,
-            uid: data.uid
         });
     });
     
 });
-
 
 // Server start
 
